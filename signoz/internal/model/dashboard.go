f2e633e937a230e4f5498549be8846676a8e253b
Priyanshu Shrivastava <priyanshu@signoz.io>
2025-04-22 23:45:06 +0530

    able to render dashboard on ui

--- signoz/internal/model/dashboard.go
+++ signoz/internal/model/dashboard.go
@@ -1,24 +1,60 @@
 package model
 
+import (
+	"strings"
+
+	"github.com/SigNoz/terraform-provider-signoz/signoz/internal/utils"
+	tfattr "github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/structure"
+)
+
 const ()
 
 var ()
 
 // Dashboard model.
 type Dashboard struct {
-	UUID     string `json:"uuid"`
-	CreateAt string `json:"createAt,omitempty"`
-	CreateBy string `json:"createBy,omitempty"`
-	UpdateAt string `json:"updateAt,omitempty"`
-	UpdateBy string `json:"updateBy,omitempty"`
+	Title       string                 `json:"title"`
+	Description string                 `json:"description"`
+	Tags        []string               `json:"tags"`
+	Layout      []string               `json:"layout"`
+	Widgets     []string               `json:"widgets"`
+	Variables   map[string]interface{} `json:"variables"`
+	CreateAt    string                 `json:"createAt,omitempty"`
+	CreateBy    string                 `json:"createBy,omitempty"`
+	UpdateAt    string                 `json:"updateAt,omitempty"`
+	UpdateBy    string                 `json:"updateBy,omitempty"`
+	UUID        string                 `json:"uuid,omitempty"`
+}
 
-	Title       string `json:"title"`
-	Description string `json:"description"`
+func (d *Dashboard) SetVariables(tfVariables types.String) error {
+	variables, err := structure.ExpandJsonFromString(tfVariables.ValueString())
+	if err != nil {
+		return err
+	}
 
-	Tags []string `json:"tags"`
+	d.Variables = variables
+	return nil
+}
 
-	Layout  []string `json:"layout"`
-	Widgets []string `json:"widgets"`
+func (d *Dashboard) SetTags(tfTags types.List) {
+	tags := utils.Map(tfTags.Elements(), func(value tfattr.Value) string {
+		return strings.Trim(value.String(), "\"")
+	})
+	d.Tags = tags
+}
 
-	Variables map[string]interface{} `json:"variables"`
+func (d *Dashboard) SetLayout(tfLayout types.List) {
+	layout := utils.Map(tfLayout.Elements(), func(value tfattr.Value) string {
+		return strings.Trim(value.String(), "\"")
+	})
+	d.Layout = layout
+}
+
+func (d *Dashboard) SetWidgets(tfWidgets types.List) {
+	widgets := utils.Map(tfWidgets.Elements(), func(value tfattr.Value) string {
+		return strings.Trim(value.String(), "\"")
+	})
+	d.Widgets = widgets
 }
