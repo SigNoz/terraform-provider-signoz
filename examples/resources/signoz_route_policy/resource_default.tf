975793cb240e9234243b33860902ca4c70fd12c1
grandwizard28 <vibhupandey28@gmail.com>
2026-06-18 15:56:59 +0530

    chore: run go generate

--- examples/resources/signoz_route_policy/resource_default.tf
+++ examples/resources/signoz_route_policy/resource_default.tf
@@ -1,5 +1,5 @@
 resource "signoz_route_policy" "default_kind" {
-  name        = "route-critical-to-oncall"
-  expression  = "service == \"payments\" && severity == \"critical\""
-  channels    = ["oncall-slack"]
+  name       = "route-critical-to-oncall"
+  expression = "service == \"payments\" && severity == \"critical\""
+  channels   = ["oncall-slack"]
 }
