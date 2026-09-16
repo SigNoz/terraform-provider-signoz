aa41f930476ae28fa1f18cb48bfff18e705f0b0b
therealpandey <vibhupandey28@gmail.com>
2026-09-16 13:39:11 +0530

    test: drop dots from notification_channel scenario names
    The v2 API rejects channel names containing dots ('must not contain
    dots'); use dashes instead.

--- tests/testdata/resources/signoz_notification_channel/00/01-slack.tf
+++ tests/testdata/resources/signoz_notification_channel/00/01-slack.tf
@@ -2,7 +2,7 @@
 # destroy. Base-only scenarios may be authored in HCL (.tf); scenarios with JSON
 # patches use a .tf.json base (see scenario 01 and ../../../README.md).
 resource "signoz_notification_channel" "scenario_00" {
-  name         = "slack.scenario-00"
+  name         = "slack-scenario-00"
   display_name = "Scenario 00 Slack"
 
   config = {
